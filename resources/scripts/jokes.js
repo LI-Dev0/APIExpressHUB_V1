8625cbb0c027e0e177c4decbe10cf1b40a0f4b1b
LIGunna93 <lilunga93@googlemail.com>
2025-08-29 01:31:31 +0200

    Jokes .ejs and .js page and script adaptations entried

---
+++ resources/scripts/jokes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+//console.dir(app);
+const port = 3000;
+// Removed static time assignment; will generate timestamp dynamically in middleware
+
+app.set('view engine', 'ejs');
+
+app.get('/jokes', (req, res) => {
+    res.render('jokes.ejs', { jokes: jokes });
+});
+
+app.use((req, res, next) => {
+    console.log("We have been pinged! Take a look!".bgCyan + `Stamp: ${new Date()}`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`.bgBlue);
+})
+
+const jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "Why don't skeletons fight each other? They don't have the guts."
+];
