91d45af11d6308f74556ab10cd15ef775d8f0138
LIGunna93 <lilunga93@googlemail.com>
2025-08-29 15:43:16 +0200

    Addition of ChuckNorris jokes and further style adaptations

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -35,3 +35,27 @@ const siteTheme = document.querySelector('body');
     siteTheme.style.backgroundColor = 'black';
     siteTheme.style.color = 'white';
     siteTheme.style.fontFamily = 'Roboto-Mono, sans-serif';
+
+const giveMeAJoke = require('give-me-a-joke');
+
+// To get a random dad joke
+//giveMeAJoke.getRandomDadJoke (function(joke) {
+    //=> console.log(joke);
+//});
+
+// To get a random Chuck Norris joke
+giveMeAJoke.getRandomCNJoke (function(joke) {
+    //=> console.log(joke);
+    const chuckNorrisJoke = document.createElement("p");
+    chuckNorrisJoke.append(joke);
+    document.body.append(chuckNorrisJoke);
+});
+
+const chuckNorrisBtn = document.querySelector(".chuckNorrisBtn");
+const chuckNorrisJoke = document.querySelector("#chuckNorrisJoke");
+
+chuckNorrisBtn.addEventListener("click", () => {
+    giveMeAJoke.getRandomCNJoke((joke) => {
+        chuckNorrisJoke.textContent = joke;
+    });
+});
