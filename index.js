cf910cf4add5dd53c3c9e4276648447ecbf21ab7
Lionel Ilunga <lilunga93@googlemail.com>
2025-10-29 18:10:09 +0000

    Index.js quotes adapted

--- index.js
+++ index.js
@@ -4,33 +4,36 @@ const app = express();
 const port = 3000;
 // Removed static time assignment; will generate timestamp dynamically in middleware
 
-const path = require('path');
+const path = require("path");
 const { title } = require("process");
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Included to serve static files such as CSS and JS
-app.use('/scripts', express.static('resources/scripts'));
-app.use('/styles', express.static('resources/styles'));
+app.use("/scripts", express.static("resources/scripts"));
+app.use("/styles", express.static("resources/styles"));
 
-app.get('/', (req, res) => {
-  res.render('home.ejs', { title: "API Express Server" });
+app.get("/", (req, res) => {
+  res.render("home.ejs", { title: "API Express Server" });
 });
 
-app.get('/jokes', (req, res) => {
-    res.render('jokes.ejs', { title: "Joke Generator" });
+app.get("/jokes", (req, res) => {
+  res.render("jokes.ejs", { title: "Joke Generator" });
 });
 
 // To log each time someone hits the joke API, we use middleware placed before the '/jokes' route handler.
 // This middleware will execute for every request to '/jokes' and log the timestamp and request details.
 
-app.use('/jokes', (req, res, next) => {
-	console.log("We have been pinged! Take a look! Stamp:", new Date(), "Headers:", req.headers);
-	next();
+app.use("/jokes", (req, res, next) => {
+  console.log(
+    "We have been pinged! Take a look! Stamp:",
+    new Date(),
+    "Headers:",
+    req.headers
+  );
+  next();
 });
 
-
 app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port} || ${new Date()}`);
-})
+  console.log(`Server is running on http://localhost:${port} || ${new Date()}`);
+});
